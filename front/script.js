6b3b33291c6a02f646690dcfc437829d0e2d9271
Andrey Nemilov <andrey.nemilov@pandadoc.com>
2019-03-24 10:05:19 +0300

    add image

--- front/script.js
+++ front/script.js
@@ -3,16 +3,13 @@ let sender;
 let recipient;
 let key;
 
-
-$("input[type=file]").change(function() {
+$("input[type=file]").change(function () {
   files = this.files;
-  console.log(files);
 });
 
 
 $(() => {
   $("#btn-submite-file").on("click", () => {
-
     let fd = new FormData();
     let files = $("#upload_file")[0].files[0];
     let key = $("#key-user").val();
@@ -23,10 +20,20 @@ $(() => {
       url: "http://localhost:5999/api/upload?userId=" + key,
       type: "POST",
       data: fd,
-      contentType: false,  
+      contentType: false,
       dataType: "json",
       processData: false,
       success(data) {
+<<<<<<< HEAD
+        $("#item").html(
+          `<img  class="get-img" src="http://127.0.0.1:8080/ipfs/${data}">`,
+        );
+      },
+      error(textStatus) {
+        $("#ajax-response").html(`ОШИБКИ AJAX запроса: ${textStatus}`);
+        console.log(`ОШИБКИ AJAX запроса: ${textStatus}`);
+      },
+=======
         $(".ajax-response").html(data);
       },
       error(textStatus) {
@@ -37,17 +44,20 @@ $(() => {
 		  else
 		  {
 			 $("#ajax-response").html(`ОШИБКИ AJAX запроса: ${textStatus}`);
-			 console.log(`ОШИБКИ AJAX запроса: ${textStatus}`); 
+			 console.log(`ОШИБКИ AJAX запроса: ${textStatus}`);
 		  }
       }
+>>>>>>> 6df0e83e9def8f05930a5269099bed8a8076f712
     });
   });
 });
 
+// change owner
 $(() => {
   $("#btn-change-owner").on("click", () => {
     const msg = $("#change-owner").serialize();
     console.log(msg);
+
     $.ajax({
       type: "GET",
       url: "https://putsreq.com/g2dkdzjKtDS20vcGIg1W",
@@ -57,7 +67,30 @@ $(() => {
       },
       error(xhr) {
         console.log(`Возникла ошибка: ${xhr.responseCode}`);
-      }
+      },
     });
+    $("#change-owner").trigger("reset");
+  });
+});
+
+// get all data
+$(() => {
+  $("#btn-get-item").on("click", () => {
+    const hash = $("#hash-item").val();
+
+    $.ajax({
+      type: "GET",
+      url: "https://putsreq.com/g2dkdzjKtDS20vcGIg1W",
+      data: hash,
+      success(data) {
+        $("#item").html(
+          `<img  class="get-img" src="http://127.0.0.1:8080/ipfs/${hash}">`,
+        );
+      },
+      error(xhr) {
+        console.log(`Возникла ошибка: ${xhr.responseCode}`);
+      },
+    });
+    $("#hash-item").val("");
   });
 });
